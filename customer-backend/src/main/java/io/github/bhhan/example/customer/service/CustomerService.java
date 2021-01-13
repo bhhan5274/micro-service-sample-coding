db02ee69e5b099decefe955e8a7372ba64d6b085
bhhan5274 <53869113+bhhan5274@users.noreply.github.com>
2021-01-13 21:47:44 +0900

    [#2 modify error]

--- customer-backend/src/main/java/io/github/bhhan/example/customer/service/CustomerService.java
+++ customer-backend/src/main/java/io/github/bhhan/example/customer/service/CustomerService.java
@@ -7,6 +7,7 @@ import io.github.bhhan.example.customer.domain.CustomerNotFoundException;
 import io.github.bhhan.example.customer.domain.CustomerRepository;
 import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 public class CustomerService {
 
   private CustomerRepository customerRepository;
@@ -15,7 +16,6 @@ public class CustomerService {
     this.customerRepository = customerRepository;
   }
 
-  @Transactional
   public Customer createCustomer(String name, Money creditLimit) {
     Customer customer  = new Customer(name, creditLimit);
     return customerRepository.save(customer);
